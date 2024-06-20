4af0876649f8db3eff71a6d45ebfef4457da9431
岳坤 <yuekundev@163.com>
2024-06-20 16:32:13 +0800

    修改：server_name

--- facefusion/uis/layouts/benchmark.py
+++ facefusion/uis/layouts/benchmark.py
@@ -64,4 +64,4 @@ def listen() -> None:
 
 def run(ui : gradio.Blocks) -> None:
 	concurrency_count = min(2, multiprocessing.cpu_count())
-	ui.queue(concurrency_count = concurrency_count).launch(show_api = False, quiet = True, inbrowser = facefusion.globals.open_browser)
+	ui.queue(concurrency_count = concurrency_count).launch(server_name='0.0.0.0', server_port=7863,show_api = False, quiet = True, inbrowser = facefusion.globals.open_browser)
--- facefusion/uis/layouts/default.py
+++ facefusion/uis/layouts/default.py
@@ -78,4 +78,4 @@ def listen() -> None:
 
 def run(ui : gradio.Blocks) -> None:
 	concurrency_count = min(8, multiprocessing.cpu_count())
-	ui.queue(concurrency_count = concurrency_count).launch(show_api = False, quiet = True, inbrowser = facefusion.globals.open_browser)
+	ui.queue(concurrency_count = concurrency_count).launch(server_name='0.0.0.0', server_port=7863,show_api = False, quiet = True, inbrowser = facefusion.globals.open_browser)
